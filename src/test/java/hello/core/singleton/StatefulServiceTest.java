3e416160beff1fc924f9ff6653e71e116fb5265b
James Seung Hyun Son <jamessoun93@gmail.com>
2022-06-14 00:08:04 +0900

    싱글톤 주의점

---
+++ src/test/java/hello/core/singleton/StatefulServiceTest.java
@@ -0,0 +1,32 @@
+package hello.core.singleton;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.annotation.Bean;
+
+class StatefulServiceTest {
+
+    @Test
+    void statefulServiceSingleton() {
+        AnnotationConfigApplicationContext ac = new AnnotationConfigApplicationContext(TestConfig.class);
+        StatefulService statefulService1 = ac.getBean(StatefulService.class);
+        StatefulService statefulService2 = ac.getBean(StatefulService.class);
+
+//        statefulService1.order("userA", 10000);
+        int userAPrice = statefulService1.order("userA", 10000);
+        statefulService2.order("userB", 20000);
+
+//        int price = statefulService1.getPrice();
+        System.out.println("price = " + userAPrice);
+//        assertThat(statefulService1.getPrice()).isEqualTo(20000);
+    }
+
+    static class TestConfig {
+
+        @Bean
+        public StatefulService statefulService() {
+            return new StatefulService();
+        }
+    }
+
+}
